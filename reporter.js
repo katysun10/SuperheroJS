8eb3e66a477f7e96866a8cf818d5a21d2d59d400
Katy Sun <katysun10@Katys-MacBook-Pro.local>
2017-06-14 17:11:52 -0600

    splitup code within superhero.js, deleted superhero.js after

---
+++ reporter.js
@@ -0,0 +1,30 @@
+
+function NewsReporter(firstName, lastName, skillLevel, requestedPay, obsHero, workPlace, heroObj, employerObj){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.skillLevel = skillLevel;
+    this.requestedPay = requestedPay;
+    this.heroObserved = obsHero;
+    if(skillLevel >= 9){
+        this.heroSecret = heroObj.secret();
+    }
+    else{
+        this.heroSecret = "...I don't know any his/her true identity, my skill is too low..."
+    }
+    var report = "After extensive observation of the events today, I, reporter, "+firstName+" "+lastName+" \nhave learned that "+obsHero+"'s signature move used to defeat arch nemesis, \n"+heroObj.archEnemy+", the "+heroObj.sigAttack+" is created from the utilization \nof his/her powers, "+heroObj.powers+".";
+    if(employerObj.employeeRate >= this.requestedPay){
+        this.giveReport = function(){
+            console.log("**This report is brought to you by the "+workPlace+"**");
+            console.log(report);
+            console.log("In addition, I know a secret about "+obsHero+". His/her true identity \nis..."+this.heroSecret+"!!!!");
+            console.log("**This report is brought to you by the "+workPlace+"**");
+        };
+    }
+    else{
+        this.giveReport = function(){
+            console.log("The pay is too low! I won't release my report");
+        }
+    }
+}; 
+
+exports.NewsReporter = NewsReporter;
