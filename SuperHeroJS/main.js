44ea6582edb8806c3b67668b937932667fd19f6c
Katy Sun <katysun10@Katys-MacBook-Pro.local>
2017-06-15 08:50:20 -0600

    made changes to station.js moved all files into new directory

---
+++ SuperHeroJS/main.js
@@ -0,0 +1,46 @@
+
+var NewsReporter = require("./reporter").NewsReporter;
+var SuperHero = require("./hero").SuperHero;
+var NewsStation = require("./station").NewsStation;
+var piggyPowers = require("./constants").piggyPowers;
+var gadgetsInBase = require("./constants").gadgetsInBase;
+
+var SpiderPig = new SuperHero("Spider Pig", piggyPowers.length, 
+                              piggyPowers, "Hammie Whammie", 
+                              "The Butcher", "Peter Porker",
+                              gadgetsInBase);
+
+var DailyBugle = new NewsStation("Daily Bugle", 5000, "Spider Pig");
+
+var JJ = new NewsReporter("Jonah", "Jameson", 
+                            9, 1000, SpiderPig, 
+                            DailyBugle);
+
+//---------------------------------------------now lets test it--------------------------------------------//
+
+//can the code give a report?
+JJ.giveReport();
+console.log("") //here for aestheticss
+
+//can the code put and store items in the superhero base?
+SpiderPig.putInBase("calory");
+console.log("") //here for aestheticss
+
+//can the code grab a gadget from the base?
+SpiderPig.getGadget("calory");
+console.log("") //here for aestheticss
+
+//can the code send a reporter out?
+DailyBugle.dispatchReporter();
+console.log("") //here for aestheticss
+
+//can the code make the sent reporter do an interview?
+DailyBugle.reporterInterview();
+console.log("") //here for aestheticss
+
+//can the code get the interview back to the news station?
+DailyBugle.getInterview();
+console.log("") //here for aestheticss
+
+//can the code print out the interview?
+DailyBugle.printInterview();
